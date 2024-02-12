b369e7c96b7f751ecaa4b1b965d729d670b17a4d
cjtcolumna <cenardjade.columna@gmail.com>
2024-02-13 01:42:22 +0800

    Added breadcrumbs

--- dashboard.php
+++ dashboard.php
@@ -1,38 +1,44 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["logged_in"])) {
+    header("Location: index.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Dashboard</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark ps-2">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Mini Inventory System</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="dashboard.php">Mini Inventory System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-           
+
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Users</a>
+                        <a class="nav-link" href="users.php">Users</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Customers</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown link
                         </a>
                         <ul class="dropdown-menu">
@@ -46,13 +52,20 @@
                     </li>
                 </ul>
             </div>
-             
+
         </div>
+    </nav>
+
+    <nav class="p-3" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active"><i class="fa-solid fa-house"></i><a href="dashboard.php">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+        </ol>
     </nav>
 
     <div class="container">
         <div class="row">
-            <div class="col mt-5">
+            <div class="col my-5">
                 <div class="d-flex justify-content-center align-items-center mx-auto total new">
                     <div class="text-center">
                         <p class="h1 p-0 m-0 total-number">100</p>
@@ -79,7 +92,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col mt-5">
+            <div class="col my-5">
                 <div class="d-flex justify-content-center align-items-center mx-auto total qualified">
                     <div class="text-center">
                         <p class="h1 p-0 m-0 total-number">20</p>
@@ -107,7 +120,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col mt-5">
+            <div class="col my-5">
                 <div class="d-flex justify-content-center align-items-center mx-auto total demo">
                     <div class="text-center">
                         <p class="h1 p-0 m-0 total-number">20</p>
@@ -135,7 +148,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col mt-5">
+            <div class="col my-5">
                 <div class="d-flex justify-content-center align-items-center mx-auto total quoted">
                     <div class="text-center">
                         <p class="h1 p-0 m-0 total-number">50</p>
@@ -163,7 +176,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col mt-5">
+            <div class="col my-5">
                 <div class="d-flex justify-content-center align-items-center mx-auto total deal">
                     <div class="text-center">
                         <p class="h1 p-0 m-0 total-number">1000</p>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+session_unset();
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,11 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Login</title>
 </head>
 
@@ -20,10 +22,10 @@ session_start();
         <div class="card-login p-5">
             <p class="h1 text-center pb-5">Login</p>
             <form class="d-grid gap-2" action="login.php" method="post">
-                <input class="form-control" type="email" name="email" placeholder="Email">
-                <input class="form-control" type="password" name="password" placeholder="Password">
+                <input class="form-control" type="email" name="email" placeholder="Email" required>
+                <input class="form-control" type="password" name="password" placeholder="Password" required>
                 <button class="btn btn-primary mt-3 mb-3" type="submit">Login</button>
-                <p class="text-center mt-3">Not a member?<a class="sign-up-link" href="#"> FREE DEMO</a></p>
+                <p class="text-center mt-3">Not a member?<a class="sign-up-link" href="#"> FREE TRIAL</a></p>
             </form>
         </div>
     </div>
